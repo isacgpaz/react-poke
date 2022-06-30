d76c541362aa87d2ce93de5d1143319d9160b15b
Isac Gomes <isacgomesp@gmail.com>
2022-06-29 22:32:13 -0300

    feat: add github link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { DotsNine, Heart, List, SlidersHorizontal } from "phosphor-react";
+import { DotsNine, GithubLogo, Heart, List } from "phosphor-react";
 import { Link } from "react-router-dom";
 import { useList } from "../hooks/useList";
 
@@ -17,13 +17,16 @@ export function Header() {
       <div className="flex justify-end gap-4">
         <button className="text-slate-900" onClick={toggleGrid}>
           {isGrid
-            ? <DotsNine weight="bold" size={24} />
-            : <List weight="regular" size={24} />}
+            ? <List weight="regular" size={24} />
+            : <DotsNine weight="bold" size={24} />}
         </button>
 
-        <button className="text-slate-900">
-          <SlidersHorizontal weight="fill" size={24} />
-        </button>
+        <a
+          href="https://github.com/isacgpaz"
+          className="flex items-center text-slate-900"
+        >
+          <GithubLogo weight="fill" size={24} />
+        </a>
 
         <button className="text-orange-500">
           <Heart weight="fill" size={32} />
